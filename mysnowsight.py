836c6db9916bba26653dffaf8d5a06036a69a640
Mahantesh Hiremath <achtwzz@mmm.com>
2023-09-26 17:52:25 +0530

    added connect me

--- mysnowsight.py
+++ mysnowsight.py
@@ -50,6 +50,7 @@ def execute_single_query(query):
 st.title("My Snowsight")
 
 # Sidebar for Snowflake credentials
+st.markdown("[![Foo](https://cdn2.iconfinder.com/data/icons/social-media-2285/512/1_Linkedin_unofficial_colored_svg-48.png)](https://www.linkedin.com/in/mahantesh-hiremath/) Connect me.")   
 st.sidebar.header("Snowflake Credentials")
 account = st.sidebar.text_input("Account")
 role = st.sidebar.text_input("Role")
