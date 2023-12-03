7c2896a312f2ede1954cbab190fc4416b70fcf93
Mahantesh Hiremath <mahanteshimath@gmail.com>
2023-12-03 16:54:23 +0530

    Update mysnowsight.py

--- mysnowsight.py
+++ mysnowsight.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import snowflake.connector
-import snowflake.snowpark
 from snowflake.snowpark.session import Session
 from snowflake.connector.pandas_tools import write_pandas
 import time
