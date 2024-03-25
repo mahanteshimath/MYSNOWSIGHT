4eb31044d2904aa984e340986bfe6a16bb65ece3
Mahantesh Hiremath <mahanteshimath@gmail.com>
2024-03-25 08:51:51 +0000

    src_cursor

--- mysnowsight.py
+++ mysnowsight.py
@@ -321,15 +321,15 @@ with tab5:
                             source_password = st.text_input("Source Password", type="password")
 
                             if st.button("Test Source Connection"):
-                                conn = test_connection(source_account, source_role, source_warehouse, source_database, source_schema, source_user, source_password)
-                                cursor = conn.cursor()
+                                src_conn = test_connection(source_account, source_role, source_warehouse, source_database, source_schema, source_user, source_password)
+                                src_cursor = src_conn.cursor()
                                 st.toast("Source Connection to Snowflake successfully!", icon='🎉')
                                 time.sleep(.5)
                                 st.balloons()
 
                                 ddl = []
                                 ddl_q = f"SELECT GET_DDL('DATABASE', '{source_database}', true) AS DDL"
-                                df_q = cursor.execute(ddl_q)
+                                df_q = src_cursor.execute(ddl_q)
                                 ddl.append(df_q.fetchone()[0])
                                 combined_ddl = "\n\n-------------------------------------------------------------------------------------------\n\n".join(ddl_statements)
                                 st.write("### Generate DDL")
